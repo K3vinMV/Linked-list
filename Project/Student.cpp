19105be7acdec54743e8246e3543b2bc6957426a
Kevin Melendez <kevinmelendez3535@gmail.com>
2024-06-17 20:59:33 -0600

    students files added

---
+++ Project/Student.cpp
@@ -0,0 +1,8 @@
+#include "Student.h"
+
+Student::Student()
+{
+    name = "";
+    age = 0;
+    //ctor
+}
---
+++ Project/Student.h
@@ -0,0 +1,16 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+#include <iostream>
+
+using namespace std;
+
+class Student
+{
+    public:
+        string name;
+        int age;
+        Student();
+
+};
+
+#endif // STUDENT_H
